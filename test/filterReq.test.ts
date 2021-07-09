c86f65b5d4a9f22048eb2b79b82f7b4d5dddc917
Minh Nguyen <mike@fruitpunchteam.com>
2021-07-09 15:25:15 -0700

    test: add cases for oak's `send`

--- test/filterReq.test.ts
+++ test/filterReq.test.ts
@@ -18,6 +18,11 @@ function nextMethod(ctx: any, next: any) {
   ctx.response.body = "Client Application";
 }
 
+async function nextMethodThatReliesOnPrivateStatesOfOak(ctx: Oak.Context, next: any) {
+    await ctx.send({root: `${Deno.cwd()}/test`, index: "index.test.html"});
+    ctx.response.status = 201;
+}
+
 describe("filterReq", () => {
   it("should continue route processing when the filter function returns false (i.e. don't filter)", async (
     done,
@@ -96,6 +101,94 @@ describe("filterReq", () => {
       filterReq: () => Promise.resolve(true),
     }));
     app.use(nextMethod);
+
+    const request = await superoak(app);
+
+    request.get("/")
+      .expect(201)
+      .end((err) => {
+        proxyServer.close();
+        done(err);
+      });
+  });
+
+  it("should continue route processing when the filter function returns false (i.e. don't filter and don't serve static files)", async (
+    done,
+  ) => {
+    const proxyServer = proxyTarget({ handlers: proxyRouteFn });
+    const proxyPort = (proxyServer.listener.addr as Deno.NetAddr).port;
+
+    const app = new Application();
+    app.use(proxy(`http://localhost:${proxyPort}`, {
+      filterReq: () => false,
+    }));
+    app.use(nextMethodThatReliesOnPrivateStatesOfOak);
+
+    const request = await superoak(app);
+
+    request.get("/")
+      .expect(200)
+      .end((err) => {
+        proxyServer.close();
+        done(err);
+      });
+  });
+
+  it("should stop route processing when the filter function returns true (i.e. filter and serve static files)", async (
+    done,
+  ) => {
+    const proxyServer = proxyTarget({ handlers: proxyRouteFn });
+    const proxyPort = (proxyServer.listener.addr as Deno.NetAddr).port;
+
+    const app = new Application();
+    app.use(proxy(`http://localhost:${proxyPort}`, {
+      filterReq: () => true,
+    }));
+    app.use(nextMethodThatReliesOnPrivateStatesOfOak);
+
+    const request = await superoak(app);
+
+    request.get("/")
+      .expect(201)
+      .end((err) => {
+        proxyServer.close();
+        done(err);
+      });
+  });
+
+  it("should continue route processing when the filter function returns Promise<false> (i.e. don't filter and don't serve static files)", async (
+    done,
+  ) => {
+    const proxyServer = proxyTarget({ handlers: proxyRouteFn });
+    const proxyPort = (proxyServer.listener.addr as Deno.NetAddr).port;
+
+    const app = new Application();
+    app.use(proxy(`http://localhost:${proxyPort}`, {
+      filterReq: () => Promise.resolve(false),
+    }));
+    app.use(nextMethodThatReliesOnPrivateStatesOfOak);
+
+    const request = await superoak(app);
+
+    request.get("/")
+      .expect(200)
+      .end((err) => {
+        proxyServer.close();
+        done(err);
+      });
+  });
+
+  it("should stop route processing when the filter function returns Promise<true> (i.e. filter and serve static files)", async (
+    done,
+  ) => {
+    const proxyServer = proxyTarget({ handlers: proxyRouteFn });
+    const proxyPort = (proxyServer.listener.addr as Deno.NetAddr).port;
+
+    const app = new Application();
+    app.use(proxy(`http://localhost:${proxyPort}`, {
+      filterReq: () => Promise.resolve(true),
+    }));
+    app.use(nextMethodThatReliesOnPrivateStatesOfOak);
 
     const request = await superoak(app);
 
