1203035d63d3aae596900fe9c2de8482fa8f361b
Minh Nguyen <mike@fruitpunchteam.com>
2021-07-13 06:59:03 -0700

    test: revert to oak@7.3.0

--- test/deps.ts
+++ test/deps.ts
@@ -1,4 +1,4 @@
-export * as Oak from "https://deno.land/x/oak@v7.7.0/mod.ts";
+export * as Oak from "https://deno.land/x/oak@v7.3.0/mod.ts";
 export * as Opine from "https://deno.land/x/opine@1.3.3/mod.ts";
 export { expect } from "https://deno.land/x/expect@v0.2.6/mod.ts";
 export { superoak } from "https://deno.land/x/superoak@4.2.0/mod.ts";
